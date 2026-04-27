78eeed7534cc7bce30208d1f10b9dda670d63f37
Rauan K <rk.kz2007@gmail.com>
2026-04-27 16:59:31 +0500

    feat(sorter): implemented basic and advanced sorting algorithms

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,2 +1,66 @@
+import java.util.Random;
+
 public class Sorter {
+    public void basicSort(int[] arr) {
+        int n = arr.length;
+        boolean swap = false;
+        for (int i = 0; i < n - 1; i++) {
+            swap = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap) {break;}
+        }
+    }
+
+    public void advancedSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    private void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public void printArray(int[] arr) {
+        for (int i: arr) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public int[] generateRandomArray(int size) {
+        Random rd = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) arr[i] = rd.nextInt(1000);
+        return arr;
+    }
 }
